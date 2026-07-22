ec6cc6b7b6270ac5c09047c2c581ccfd5337d469
AugustoUCE <aysalazar@uce.edu.ec>
2026-07-21 21:25:58 -0500

    baseshelllayout loginView

---
+++ src/layouts/BaseShellLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-background">
+    <!-- Elementos decorativos -->
+    <div class="bg-gradient"></div>
+    <div class="bg-dots"></div>
+
+    <!-- Contenido principal -->
+    <main class="app-content">
+      <section class="linkme-card">
+          <!-- Aquí cambia LoginView por HomeView -->
+        <RouterView />
+      </section>
+    </main>
+  </div>
+</template>
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
