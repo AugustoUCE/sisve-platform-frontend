489b413b0182c445f0bf4f7e1785faff911e9ca0
jhonjaxde <jjarteaga@uce.edu.ec>
2026-07-22 21:38:17 -0500

    votebox

---
+++ src/components/voteBox/voteBox.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+defineProps<{
+    selected:boolean
+}>()
+
+const emit = defineEmits<{
+    (e:"select"):void
+}>()
+
+</script>
+
+<template>
+
+<div
+    class="vote-box"
+    :class="{active:selected}"
+    @click="emit('select')"
+>
+
+    <svg
+        class="mark"
+        viewBox="0 0 100 100"
+    >
+        <line
+            x1="80"
+            y1="20"
+            x2="20"
+            y2="80"
+        />
+    </svg>
+
+</div>
+
+</template>
